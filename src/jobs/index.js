3ff3cb8a90608b1bdd01a31d9055564213be638f
Mateusz Ordon <98200397+Namencanto@users.noreply.github.com>
2024-07-22 11:38:03 +0200

    Update index.js

--- src/jobs/index.js
+++ src/jobs/index.js
@@ -2,10 +2,10 @@ import cron from 'node-cron'
 import { checkDeposits } from './checkDeposits.js' 
 
 export function setupCronJobs(bot) {
-  console.log('Running checkDeposits job...');
-  checkDeposits(bot);
-  cron.schedule('* * * * *', () => {
-    checkDeposits(bot);
-  });
+  // console.log('Running checkDeposits job...');
+  // checkDeposits(bot);
+  // cron.schedule('* * * * *', () => {
+    // checkDeposits(bot);
+  // });
 }
 
